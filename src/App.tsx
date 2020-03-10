ac4f688dd755bb14eeb37794d62e4d8907c9c365
David Obodo <obododavid5@gmail.com>
2020-03-10 09:45:19 +0100

    export global styles to seperate file

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,12 @@
 import React from 'react';
-import { createGlobalStyle } from 'styled-components';
+
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import { Routes } from './routing/routes';
 
+import { GlobalStyles } from './GlobalStyles';
 
-const GlobalStyles = createGlobalStyle`
-  *, 
-  *:before, 
-  *:after{
-    margin: 0;
-    padding: 0;
-    box-sizing: border-box;
-  }
 
-  html{
-    font-size: 62.5%;
-  }
 
-  body{
-    font-size: 1.4rem;
-    font-family: sans-serif;
-  }
-
-  h1{
-    font-size: 70px;
-    font-weight: 600;
-  }
-`
 
 const App = () => {
   return (
---
+++ src/GlobalStyles.ts
@@ -0,0 +1,25 @@
+import { createGlobalStyle } from 'styled-components';
+
+export const GlobalStyles = createGlobalStyle`
+  *, 
+  *:before, 
+  *:after{
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+  html{
+    font-size: 62.5%;
+  }
+
+  body{
+    font-size: 1.4rem;
+    font-family: sans-serif;
+  }
+
+  h1{
+    font-size: 70px;
+    font-weight: 600;
+  }
+`
