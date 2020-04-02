fff1aaa2642c44525ae90aed83f78bf3993744dc
David Obodo <obododavid5@gmail.com>
2020-04-02 16:11:19 +0100

    complete login page styles for mobile

--- src/components/pages/loginPage/loginPage.tsx
+++ src/components/pages/loginPage/loginPage.tsx
@@ -1,13 +1,58 @@
 import React from "react";
 import styled from "styled-components";
 import Layout from "../../shared/layout/layout";
+import Input from "../../shared/input/input";
+import Button from "../../shared/button/button";
 
-const LoginContainer = styled.div``;
+const ipnut_fields = [
+    {
+        label: "Email",
+        type: "email"
+    },
+    {
+        label: "Password",
+        type: "password"
+    }
+];
+
+const LoginContainer = styled.div`
+    margin-top: 50px;
+    margin-bottom: 100px;
+
+    form {
+        border: 1px solid ${props => props.theme.primaryColor};
+        padding: 30px 10px;
+
+        h2 {
+            font-weight: 400;
+            margin-bottom: 20px;
+        }
+    }
+
+    //-------------------------------------------------------------------------
+    //resets on some components on this page
+    //-------------------------------------------------------------------------
+
+    h6 {
+        font-size: 14px;
+        margin-bottom: 10px;
+        font-weight: 800;
+    }
+`;
 
 const Login = () => {
     return (
         <Layout isFooterPresent>
-            <LoginContainer>dey there</LoginContainer>
+            <LoginContainer>
+                <form action="">
+                    <h2>Login</h2>
+                    {ipnut_fields.map((field, i) => {
+                        const { label, type } = field;
+                        return <Input key={i} label={label} type={type} />;
+                    })}
+                    <Button blue_small_text>Login</Button>
+                </form>
+            </LoginContainer>
         </Layout>
     );
 };
