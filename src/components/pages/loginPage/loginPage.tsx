cb421ca4bfd68c436bafdc0d79201b56f28330ed
David Obodo <obododavid5@gmail.com>
2020-04-06 08:27:04 +0100

    -create saga for both auth pages -adjust some database rules

--- src/components/pages/loginPage/loginPage.tsx
+++ src/components/pages/loginPage/loginPage.tsx
@@ -1,62 +1,49 @@
-import React from "react";
-import styled from "styled-components";
+import React, { useState } from "react";
+import { useDispatch } from "react-redux";
 import Layout from "../../shared/layout/layout";
 import Input from "../../shared/input/input";
 import Button from "../../shared/button/button";
-import { devices } from "../../../styling/devices";
+import { requestUserLoginStart } from "../../../redux/actions/auth";
+import { LoginContainer } from "./style";
 
 const ipnut_fields = [
     {
         label: "Email",
-        type: "email"
+        type: "email",
+        name: "email"
     },
     {
         label: "Password",
-        type: "password"
+        type: "password",
+        name: "password"
     }
 ];
 
-const LoginContainer = styled.div`
-    margin-top: 50px;
-    margin-bottom: 100px;
-
-    form {
-        border: 1px solid ${props => props.theme.primaryColor};
-        padding: 30px 10px;
-
-        @media ${devices.tablet} {
-            max-width: 500px;
-            margin: 0 auto;
-        }
-
-        h2 {
-            font-weight: 400;
-            margin-bottom: 20px;
-        }
-
-        div {
-            @media ${devices.tablet} {
-                width: 100%;
-            }
-        }
-    }
-
-    //-------------------------------------------------------------------------
-    //resets on some components on this page
-    //-------------------------------------------------------------------------
-
-    h6 {
-        font-size: 14px;
-        margin-bottom: 10px;
-        font-weight: 800;
-    }
-`;
-
 const Login = () => {
+    const dispatch = useDispatch();
+    const [userDetails, setUserDetails] = useState({
+        firstName: "",
+        lastName: "",
+        email: "",
+        password: ""
+    });
+    const handleOnChange = e => {
+        const { name, value } = e.target;
+        setUserDetails({ ...userDetails, [name]: value });
+    };
+    const handleOnsubmit = e => {
+        e.preventDefault();
+        dispatch(requestUserLoginStart(userDetails));
+    };
     return (
         <Layout isFooterPresent>
             <LoginContainer>
-                <form action="">
+                <form
+                    action=""
+                    onSubmit={handleOnsubmit}
+                    onChange={handleOnChange}
+                    noValidate
+                >
                     <h2>Login</h2>
                     {ipnut_fields.map((field, i) => {
                         const { label, type } = field;
