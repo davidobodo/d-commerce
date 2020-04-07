9a679a05f2170733aaafcfee9bd290e1f7268a5b
David Obodo <obododavid5@gmail.com>
2020-04-07 05:20:39 +0100

    -add spinner to auth pages to give user a visual cue that their details are being processed

--- src/redux/reducers/index.ts
+++ src/redux/reducers/index.ts
@@ -1,6 +1,10 @@
 import { combineReducers } from "redux";
 import cart from "./all/cart/cart";
+import signup from "./all/signup/signup";
+import login from "./all/login/login";
 
 export default combineReducers({
-    cart
+    cart,
+    signup,
+    login
 });
--- src/redux/sagas/index.ts
+++ src/redux/sagas/index.ts
@@ -1,6 +1,7 @@
 import { all } from "redux-saga/effects";
 import signUpSaga from "./all/signup";
+import loginSaga from "./all/login";
 
 export default function* rootSaga() {
-    yield all([signUpSaga()]);
+    yield all([signUpSaga(), loginSaga()]);
 }
