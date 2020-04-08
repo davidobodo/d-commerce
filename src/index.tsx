df4478d36b5acae3d658f6a084db7668b6117d98
David Obodo <obododavid5@gmail.com>
2020-04-08 19:12:16 +0100

    env file now working

--- src/index.tsx
+++ src/index.tsx
@@ -23,25 +23,25 @@ import firebase from "firebase/app";
 import "firebase/auth";
 import "firebase/firestore";
 
-// const firebaseConfig = {
-//     apiKey: process.env.D_COMMERCE_apiKey,
-//     authDomain: process.env.D_COMMERCE_authDomain,
-//     databaseURL: process.env.D_COMMERCE_databaseURL,
-//     projectId: process.env.D_COMMERCE_projectId,
-//     storageBucket: process.env.D_COMMERCE_storageBucket,
-//     messagingSenderId: process.env.D_COMMERCE_messagingSenderId,
-//     appId: process.env.D_COMMERCE_appId
-// };
-
 const firebaseConfig = {
-    apiKey: "AIzaSyAZbe_EMWbVeFMFlJIJNwnDRtB2OMxQOX8",
-    authDomain: "d-commerce-99633.firebaseapp.com",
-    databaseURL: "https://d-commerce-99633.firebaseio.com",
-    projectId: "d-commerce-99633",
-    storageBucket: "d-commerce-99633.appspot.com",
-    messagingSenderId: "308038401229",
-    appId: "1:308038401229:web:6b59c6c56e5a46b9bfbd84"
+    apiKey: process.env.REACT_APP_apiKey,
+    authDomain: process.env.REACT_APP_authDomain,
+    databaseURL: process.env.REACT_APP_databaseURL,
+    projectId: process.env.REACT_APP_projectId,
+    storageBucket: process.env.REACT_APP_storageBucket,
+    messagingSenderId: process.env.REACT_APP_messagingSenderId,
+    appId: process.env.REACT_APP_appId
 };
+
+// const firebaseConfig = {
+//     apiKey: "AIzaSyAZbe_EMWbVeFMFlJIJNwnDRtB2OMxQOX8",
+//     authDomain: "d-commerce-99633.firebaseapp.com",
+//     databaseURL: "https://d-commerce-99633.firebaseio.com",
+//     projectId: "d-commerce-99633",
+//     storageBucket: "d-commerce-99633.appspot.com",
+//     messagingSenderId: "308038401229",
+//     appId: "1:308038401229:web:6b59c6c56e5a46b9bfbd84"
+// };
 
 const firebase_init = firebase.initializeApp(firebaseConfig);
 
