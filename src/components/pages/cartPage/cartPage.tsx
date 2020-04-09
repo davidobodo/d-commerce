ba9e73417b0f77168777d0a73f9f1cddd18aa466
David Obodo <obododavid5@gmail.com>
2020-04-09 18:39:08 +0100

    getting product id and edited quantity

--- src/components/pages/cartPage/cartPage.tsx
+++ src/components/pages/cartPage/cartPage.tsx
@@ -14,6 +14,7 @@ import { cartItemInterface } from "../../../redux/reducers/all/cart/cartInterfac
 import { editItemQuantity, deleteCartItem } from "../../../redux/actions/cart";
 
 const CartPage = () => {
+    console.log("component renders");
     const [quantity, setQuantity] = useState();
 
     const cart = useSelector(state => state.cart, shallowEqual);
@@ -29,6 +30,7 @@ const CartPage = () => {
             const total = Object.values(cart)
                 .map((item: cartItemInterface) => {
                     const { price, productQuantity } = item;
+                    console.log({ price }, { productQuantity });
                     const item_total_price = (
                         parseFloat(price.slice(1)) * parseInt(productQuantity)
                     ).toFixed(2);
@@ -168,7 +170,7 @@ const CartPage = () => {
                                     <tr>
                                         <td>Subtotal:</td>
                                         <td className="subtotal-value">
-                                            {renderTotalPrice() as any}
+                                            {/* {renderTotalPrice() as any} */}
                                         </td>
                                     </tr>
                                     <tr>
