0ce6cc96b9f9df4cd86284ef275fc7e2062d6151
David Obodo <obododavid5@gmail.com>
2020-04-11 02:46:43 +0100

    authentication for signup page working

--- src/components/pages/loginPage/loginPage.tsx
+++ src/components/pages/loginPage/loginPage.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import { useDispatch, useSelector, shallowEqual } from "react-redux";
 import { Redirect } from "react-router-dom";
+import * as EmailValidator from "email-validator";
 import Layout from "../../shared/layout/layout";
 import Input from "../../shared/input/input";
 import Button from "../../shared/button/button";
@@ -8,21 +9,13 @@ import Spinner from "../../shared/spinner/spinner";
 import { requestUserLoginStart } from "../../../redux/actions/auth";
 import { LoginContainer } from "./style";
 
-const ipnut_fields = [
-    {
-        label: "Email",
-        type: "email",
-        name: "email"
-    },
-    {
-        label: "Password",
-        type: "password",
-        name: "password"
-    }
-];
-
 const Login = () => {
     const dispatch = useDispatch();
+    const [emailHasError, setEmailHasError] = useState(false);
+    const [passwordHasError, setPasswordHasError] = useState(false);
+
+    const [emailErrorMessage, setEmailErrorMessage] = useState();
+    const [passwordErrorMessage, setPasswordErrorMessage] = useState();
 
     const { isLoading, firebase } = useSelector(state => {
         return {
@@ -41,14 +34,67 @@ const Login = () => {
         setUserDetails({ ...userDetails, [name]: value });
     };
 
-    const handleOnsubmit = e => {
-        e.preventDefault();
-        dispatch(requestUserLoginStart(userDetails));
+    const handleOnKeyDown = e => {
+        const { name } = e.target;
+
+        if (e.key === "Enter") return;
+
+        if (name === "email") {
+            setEmailHasError(false);
+        }
+
+        if (name === "password" && userDetails.password.length > 6) {
+            setPasswordHasError(false);
+        } else {
+            setPasswordHasError(true);
+        }
     };
 
     if (firebase.auth.uid) {
         return <Redirect to="/" />;
     }
+
+    const handleValidateForm = () => {
+        const { email, password } = userDetails;
+        const letters = /^[A-Za-z]+$/;
+
+        //email
+        if (!EmailValidator.validate(email)) {
+            setEmailHasError(true);
+            setEmailErrorMessage("Insert a valid email");
+        }
+
+        //password
+        if (password.length <= 6) {
+            setPasswordHasError(true);
+            setPasswordErrorMessage(
+                "password must be greater than six characters"
+            );
+        }
+    };
+
+    const handleOnsubmit = e => {
+        handleValidateForm();
+        e.preventDefault();
+        dispatch(requestUserLoginStart(userDetails));
+    };
+
+    const ipnut_fields = [
+        {
+            label: "Email",
+            type: "email",
+            name: "email",
+            inputHasError: emailHasError,
+            inputErrorMessage: emailErrorMessage
+        },
+        {
+            label: "Password",
+            type: "password",
+            name: "password",
+            inputHasError: passwordHasError,
+            inputErrorMessage: passwordErrorMessage
+        }
+    ];
     return (
         <Layout isFooterPresent>
             {isLoading && <Spinner />}
@@ -57,17 +103,26 @@ const Login = () => {
                     action=""
                     onSubmit={handleOnsubmit}
                     onChange={handleOnChange}
+                    onKeyDown={handleOnKeyDown}
                     noValidate
                 >
                     <h2>Login</h2>
                     {ipnut_fields.map((field, i) => {
-                        const { label, type, name } = field;
+                        const {
+                            label,
+                            type,
+                            name,
+                            inputHasError,
+                            inputErrorMessage
+                        } = field;
                         return (
                             <Input
                                 key={i}
                                 label={label}
                                 type={type}
                                 name={name}
+                                hasError={inputHasError}
+                                errorMessage={inputErrorMessage}
                             />
                         );
                     })}
