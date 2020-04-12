9f76336cd9337af049ff1f172d9108ef401fe36b
David Obodo <obododavid5@gmail.com>
2020-04-12 08:30:13 +0100

    solve scrolling problem when moving to a different page

--- src/App.tsx
+++ src/App.tsx
@@ -6,12 +6,14 @@ import { Routes } from "./routing/routes";
 
 import { GlobalStyles } from "./styling/GlobalStyles";
 import { theme } from "./styling/themes";
+import ScrollToTop from "./components/shared/scrollToTop/scrollToTop";
 
 const App = () => {
     return (
         <ThemeProvider theme={theme}>
             <GlobalStyles />
             <Router>
+                <ScrollToTop />
                 <Switch>
                     {Routes.map((route, i) => {
                         const { component, exact, path } = route;
