75406e38204b1c49202be3df26b229a00b1267bf
David Obodo <obododavid5@gmail.com>
2020-04-21 07:47:03 +0100

    add more products

--- src/constants/AllProducts.ts
+++ src/constants/AllProducts.ts
@@ -3,73 +3,36 @@ import shoe2 from "../assets/img/products/shoes/brown-merrell.jpg";
 import shoe3 from "../assets/img/products/shoes/pink-hells.jpg";
 import shoe4 from "../assets/img/products/shoes/red-heels.jpg";
 import shoe5 from "../assets/img/products/shoes/snickers.png";
+import shoe6 from "../assets/img/products/shoes/casual-leather.jpg";
+import shoe7 from "../assets/img/products/shoes/dress-brown.jpg";
+import shoe8 from "../assets/img/products/shoes/italian-leather.jpg";
+import shoe9 from "../assets/img/products/shoes/ash-heels.jpg";
+import shoe10 from "../assets/img/products/shoes/colored-heels.jpg";
+import shoe11 from "../assets/img/products/shoes/transparent-sandals.jpg";
+import shoe12 from "../assets/img/products/shoes/head-black.jpg";
+import shoe13 from "../assets/img/products/shoes/black-red.jpg";
+import shoe14 from "../assets/img/products/shoes/black-clarks.jpg";
+import shoe15 from "../assets/img/products/shoes/brown-cooperate.jpg";
 
 import shirt1 from "../assets/img/products/shirts/black-ash.jpg";
 import shirt2 from "../assets/img/products/shirts/black-polo.jpg";
 import shirt3 from "../assets/img/products/shirts/mixed.jpg";
 import shirt4 from "../assets/img/products/shirts/pack.jpg";
 import shirt5 from "../assets/img/products/shirts/white-bulb.jpg";
+import shirt6 from "../assets/img/products/shirts/shirt-cooperate-1.jpg";
+import shirt7 from "../assets/img/products/shirts/shirt-cooperate-2.jpg";
+import shirt8 from "../assets/img/products/shirts/shirt-cooperate-3.jpg";
+import shirt9 from "../assets/img/products/shirts/shirt-cooperate-4.jpg";
+import shirt10 from "../assets/img/products/shirts/shirt-cooperate-5.jpg";
 
 export const myproducts = [
-    {
-        id: 1,
-        image:
-            "https://images.unsplash.com/photo-1525966222134-fcfa99b8ae77?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=943&q=80",
-        name: "Vans shoe",
-        rating: 2,
-        price: "$20.98",
-        sizes: [32, 35, 40, 42, 25],
-        description:
-            "A shoe is an item of footwear intended to protect and comfort the human foot. Shoes are also used as an item of decoration and fashion. The design of shoes has varied enormously through time and from culture to culture, with appearance originally being tied to function."
-    },
-    {
-        id: 2,
-        image:
-            "https://images.unsplash.com/photo-1521572163474-6864f9cf17ab?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1600&q=80",
-        name: "delux T-shirt",
-        rating: 4,
-        price: "$10",
-        sizes: ["Large", "Medium", "Small", "X-Large", "X-small"],
-        description:
-            "A shirt is a cloth garment for the upper body Originally an undergarment worn exclusively by men, it has become, in American English, a catch-all term for a "
-    },
-    {
-        id: 3,
-        image:
-            "https://images.unsplash.com/photo-1461141346587-763ab02bced9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=3048&q=80",
-        name: "Vlex-designs",
-        rating: 5,
-        price: "$200.98",
-        description:
-            "Apple Watch is the ultimate device for a healthy life. Choose from all the latest models like Apple Watch Series 5 with the Always-On Retina display."
-    },
-    {
-        id: 4,
-        image:
-            "https://images.unsplash.com/photo-1534030347209-467a5b0ad3e6?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=934&q=80",
-        name: "coutour brande",
-        rating: 3,
-        price: "$50.98",
-        sizes: ["Large", "Medium", "Small", "X-Large", "X-small"],
-        description:
-            "a set of outer clothes made of the same fabric and designed to be worn together, typically consisting of a jacket and trousers or a jacket and skirt."
-    },
-    {
-        id: 5,
-        image:
-            "https://images.unsplash.com/photo-1553704571-c32d20e6c74f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2250&q=80",
-        name: "Louis Vuiton",
-        rating: 0,
-        price: "$155.98",
-        description:
-            "A belt is a flexible band or strap, typically made of leather or heavy cloth and worn around the waist, that is usually of less circumference than the hips underneath. Belts are used to secure or hold up clothing, like trousers"
-    },
     {
         id: "shoe/1",
         image: shoe1,
         name: "Boss cv",
         rating: 5,
         price: "$200.54",
+        category: ["shoe", "brown", "sports", "male"],
         description:
             "A shoe is an item of footwear intended to protect and comfort the human foot. Shoes are also used as an item of decoration and fashion. The design of shoes has varied enormously through time "
     },
@@ -79,6 +42,7 @@ export const myproducts = [
         name: "Merrell shoe",
         rating: 3,
         price: "$193.26",
+        category: ["shoe", "brown", "sports", "male"],
         description:
             "A shoe is an item of footwear intended to protect and comfort the human foot. Shoes are also used as an item of decoration and fashion. The design of shoes has varied enormously through time "
     },
@@ -88,6 +52,7 @@ export const myproducts = [
         name: "L-V",
         rating: 4,
         price: "$549.60",
+        category: ["female", "heels", "pink"],
         description:
             "A shoe is an item of footwear intended to protect and comfort the human foot. Shoes are also used as an item of decoration and fashion. The design of shoes has varied enormously through time "
     },
@@ -97,6 +62,7 @@ export const myproducts = [
         name: "Princess",
         rating: 2,
         price: "$100.97",
+        category: ["female", "heels", "red"],
         description:
             "A shoe is an item of footwear intended to protect and comfort the human foot. Shoes are also used as an item of decoration and fashion. The design of shoes has varied enormously through time "
     },
@@ -106,6 +72,107 @@ export const myproducts = [
         name: "Snickers",
         rating: 3,
         price: "$56",
+        category: ["shoe", "all-stars", "sports", "male"],
+        description:
+            "A shoe is an item of footwear intended to protect and comfort the human foot. Shoes are also used as an item of decoration and fashion. The design of shoes has varied enormously through time "
+    },
+    {
+        id: "shoe/6",
+        image: shoe6,
+        name: "Casual-leather",
+        rating: 5,
+        price: "$560",
+        category: ["shoe", "brown", "leather"],
+        description:
+            "A shoe is an item of footwear intended to protect and comfort the human foot. Shoes are also used as an item of decoration and fashion. The design of shoes has varied enormously through time "
+    },
+    {
+        id: "shoe/7",
+        image: shoe7,
+        name: "dress-brown",
+        rating: 4,
+        price: "$280",
+        category: ["shoe", "brown", "leather"],
+        description:
+            "A shoe is an item of footwear intended to protect and comfort the human foot. Shoes are also used as an item of decoration and fashion. The design of shoes has varied enormously through time "
+    },
+    {
+        id: "shoe/8",
+        image: shoe8,
+        name: "Italian-leather",
+        rating: 3,
+        price: "$111",
+        category: ["shoe", "brown", "leather"],
+        description:
+            "A shoe is an item of footwear intended to protect and comfort the human foot. Shoes are also used as an item of decoration and fashion. The design of shoes has varied enormously through time "
+    },
+    {
+        id: "shoe/9",
+        image: shoe9,
+        name: "Thick heel bag",
+        rating: 3,
+        price: "$199",
+        category: ["heels", "female", "ash"],
+        description:
+            "A shoe is an item of footwear intended to protect and comfort the human foot. Shoes are also used as an item of decoration and fashion. The design of shoes has varied enormously through time "
+    },
+    {
+        id: "shoe/10",
+        image: shoe10,
+        name: "Colored heels",
+        rating: 4,
+        price: "$111",
+        category: ["heels", "female", "ash"],
+        description:
+            "A shoe is an item of footwear intended to protect and comfort the human foot. Shoes are also used as an item of decoration and fashion. The design of shoes has varied enormously through time "
+    },
+    {
+        id: "shoe/11",
+        image: shoe11,
+        name: "Transaparent sexy sandal",
+        rating: 5,
+        price: "$9000",
+        category: ["heels", "female", "ash"],
+        description:
+            "A shoe is an item of footwear intended to protect and comfort the human foot. Shoes are also used as an item of decoration and fashion. The design of shoes has varied enormously through time "
+    },
+    {
+        id: "shoe/12",
+        image: shoe12,
+        name: "Head black sports kit",
+        rating: 5,
+        price: "$900",
+        category: ["sports", "male"],
+        description:
+            "A shoe is an item of footwear intended to protect and comfort the human foot. Shoes are also used as an item of decoration and fashion. The design of shoes has varied enormously through time "
+    },
+    {
+        id: "shoe/13",
+        image: shoe13,
+        name: "Black Red snickers",
+        rating: 5,
+        price: "$550",
+        category: ["sports", "male", "black"],
+        description:
+            "A shoe is an item of footwear intended to protect and comfort the human foot. Shoes are also used as an item of decoration and fashion. The design of shoes has varied enormously through time "
+    },
+    {
+        id: "shoe/14",
+        image: shoe14,
+        name: "Black Carks men's wear",
+        rating: 5,
+        price: "$1200",
+        category: ["sports", "male", "black"],
+        description:
+            "A shoe is an item of footwear intended to protect and comfort the human foot. Shoes are also used as an item of decoration and fashion. The design of shoes has varied enormously through time "
+    },
+    {
+        id: "shoe/15",
+        image: shoe15,
+        name: "Brown luxers",
+        rating: 5,
+        price: "$600",
+        category: ["sports", "male", "black"],
         description:
             "A shoe is an item of footwear intended to protect and comfort the human foot. Shoes are also used as an item of decoration and fashion. The design of shoes has varied enormously through time "
     },
@@ -115,6 +182,7 @@ export const myproducts = [
         name: "Long sleeve T-shirt",
         rating: 5,
         price: "$56",
+        category: ["t-shirt", "male"],
         description:
             "A shirt is a cloth garment for the upper body Originally an undergarment worn exclusively by men, it has become, in American English, a catch-all term for a "
     },
@@ -124,6 +192,7 @@ export const myproducts = [
         name: "Bad Gang T Shirt - Black",
         rating: 2,
         price: "$78",
+        category: ["t-shirt", "male"],
         description:
             "A shirt is a cloth garment for the upper body Originally an undergarment worn exclusively by men, it has become, in American English, a catch-all term for a "
     },
@@ -133,6 +202,7 @@ export const myproducts = [
         name: "Birrs Casual Tee",
         rating: 4,
         price: "$20.86",
+        category: ["t-shirt", "male"],
         description:
             "A shirt is a cloth garment for the upper body Originally an undergarment worn exclusively by men, it has become, in American English, a catch-all term for a "
     },
@@ -142,6 +212,7 @@ export const myproducts = [
         name: "4-in-1 quality Men's polo",
         rating: 5,
         price: "$200.87",
+        category: ["t-shirt", "male"],
         description:
             "A shirt is a cloth garment for the upper body Originally an undergarment worn exclusively by men, it has become, in American English, a catch-all term for a "
     },
@@ -151,6 +222,57 @@ export const myproducts = [
         name: "Men's white stylish T-shirt",
         rating: 3,
         price: "$100.02",
+        category: ["t-shirt", "male"],
+        description:
+            "A shirt is a cloth garment for the upper body Originally an undergarment worn exclusively by men, it has become, in American English, a catch-all term for a "
+    },
+    {
+        id: "shirt/6",
+        image: shirt6,
+        name: "White black office shirt",
+        rating: 4,
+        price: "$80",
+        category: ["shirt", "male", "cooperate"],
+        description:
+            "A shirt is a cloth garment for the upper body Originally an undergarment worn exclusively by men, it has become, in American English, a catch-all term for a "
+    },
+    {
+        id: "shirt/7",
+        image: shirt7,
+        name: "Pink black T-shirt",
+        rating: 3,
+        price: "$50.02",
+        category: ["shirt", "male", "cooperate"],
+        description:
+            "A shirt is a cloth garment for the upper body Originally an undergarment worn exclusively by men, it has become, in American English, a catch-all term for a "
+    },
+    {
+        id: "shirt/8",
+        image: shirt8,
+        name: "Stylish office shirt",
+        rating: 3,
+        price: "$100.02",
+        category: ["shirt", "male", "cooperate"],
+        description:
+            "A shirt is a cloth garment for the upper body Originally an undergarment worn exclusively by men, it has become, in American English, a catch-all term for a "
+    },
+    {
+        id: "shirt/9",
+        image: shirt9,
+        name: "Blue cooperate shirt",
+        rating: 3,
+        price: "$100.02",
+        category: ["shirt", "male", "cooperate"],
+        description:
+            "A shirt is a cloth garment for the upper body Originally an undergarment worn exclusively by men, it has become, in American English, a catch-all term for a "
+    },
+    {
+        id: "shirt/10",
+        image: shirt10,
+        name: "Brown burberry",
+        rating: 3,
+        price: "$100.02",
+        category: ["shirt", "male", "cooperate"],
         description:
             "A shirt is a cloth garment for the upper body Originally an undergarment worn exclusively by men, it has become, in American English, a catch-all term for a "
     }
