1364d04d411d67d184d3cd7b14fff3d350ddb0a8
David Obodo <obododavid5@gmail.com>
2020-05-13 13:07:24 +0100

    written test for enabled button and disabled button

--- src/components/shared/button/button.test.tsx
+++ src/components/shared/button/button.test.tsx
@@ -10,3 +10,21 @@ it('Button should render text passed as child', () => {
     const { queryByText } = render(<Button children={'submit'} />)
     expect(queryByText('submit')).toBeTruthy();
 })
+
+it('Button should be enabled', () => {
+    const props = {
+        children: 'submit',
+        disabled: false
+    }
+    const { queryByText } = render(<Button {...props} />)
+    expect(queryByText('submit')).toBeEnabled();
+})
+
+it('Button should be disabled', () => {
+    const props = {
+        children: 'submit',
+        disabled: true
+    }
+    const { queryByText } = render(<Button {...props} />)
+    expect(queryByText('submit')).toBeDisabled();
+})
