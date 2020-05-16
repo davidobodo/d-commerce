1eee6c45a4023c321066e7d87b7cc693ab4105bc
David Obodo <obododavid5@gmail.com>
2020-05-16 10:32:13 +0100

    succesfully outputting loggedinusers name

--- src/components/shared/navbar/navbar.tsx
+++ src/components/shared/navbar/navbar.tsx
@@ -1,20 +1,27 @@
-import React from "react";
+import React, { FunctionComponent } from "react";
 
 import { Link } from "react-router-dom";
 import { useSelector, shallowEqual, useDispatch } from "react-redux";
 import { NavbarContainer } from "./style";
 import { signOutStart } from "../../../redux/actions/auth";
-import { userInfo } from "os";
+import { firestoreConnect } from 'react-redux-firebase'
+
 
 const Navbar = () => {
     const dispatch = useDispatch();
-    const firebase = useSelector(state => state.firebaseReducer, shallowEqual);
+    // const firebase = useSelector(state => state.firebaseReducer, shallowEqual);
     const userInfo = useSelector(state => state.signup.data)
+    const allUsers = useSelector(state => state.firestoreReducer.ordered.users);
     const handleSignout = () => {
         dispatch(signOutStart());
     };
 
-    const { auth, profile } = firebase;
+    let loggedInUser;
+    if (allUsers && userInfo) {
+        loggedInUser = allUsers.filter(user => userInfo.localId === user.id)
+    }
+
+    // const { auth, profile } = firebase;
 
     const renderAuthLinks = () => {
         if (userInfo) {
@@ -22,7 +29,7 @@ const Navbar = () => {
                 return (
                     <>
                         <h4 className="nav__links__username">
-                            Hi, {profile.firstName}
+                            Hi, {loggedInUser[0] && loggedInUser[0].firstName}
                         </h4>
                         <div className="nav__links__auth">
                             <Link to="/" onClick={handleSignout}>
@@ -60,4 +67,4 @@ const Navbar = () => {
     );
 };
 
-export default Navbar;
+export default firestoreConnect([{ collection: 'users' }])(Navbar) as any;
