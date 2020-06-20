a9280bfb3e099f81da33334bd8201d8eb909b5fd
David Obodo <obododavid5@gmail.com>
2020-06-20 03:45:44 +0100

    complete basic styling for dashboard

--- src/components/pages/dashboardPage/style.ts
+++ src/components/pages/dashboardPage/style.ts
@@ -3,7 +3,7 @@ import styled from "styled-components";
 export const DashboardPageContainer = styled.div`
     .header {
         border-bottom: 1px solid ${(props) => props.theme.primaryColor};
-        margin-bottom: 20px;
+        margin-bottom: 60px;
         padding-bottom: 20px;
         h1 {
             font-weight: 400;
@@ -14,8 +14,15 @@ export const DashboardPageContainer = styled.div`
     .section {
         display: flex;
         flex-direction: column;
+        align-items: center;
         border-bottom: 1px solid ${(props) => props.theme.primaryColor};
-        margin-bottom: 20px;
-        padding-bottom: 20px;
+        margin-bottom: 40px;
+        padding-bottom: 40px;
+
+        div {
+            span {
+                margin-right: 20px;
+            }
+        }
     }
 `;
