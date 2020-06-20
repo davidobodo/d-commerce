5646032695bb22a188219a7048d5733de8d836d8
David Obodo <obododavid5@gmail.com>
2020-06-20 03:22:22 +0100

    create dashboard header

--- src/components/pages/dashboardPage/dashboardPage.tsx
+++ src/components/pages/dashboardPage/dashboardPage.tsx
@@ -1,8 +1,16 @@
 import React from "react";
 import Layout from "../../shared/layout/layout";
 
-const DashboardPage = () => {
-    return <Layout isFooterPresent={true}>this is the dashboard</Layout>;
-};
+import { DashboardPageContainer } from "./style";
+
+const DashboardPage = () => (
+    <Layout isFooterPresent={true}>
+        <DashboardPageContainer>
+            <div className="header">
+                <h1>Dashboard</h1>
+            </div>
+        </DashboardPageContainer>
+    </Layout>
+);
 
 export default DashboardPage;
