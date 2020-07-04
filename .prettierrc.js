286d6076dc788a428fbe0363dbd3fd377539917f
David Obodo <obododavid5@gmail.com>
2020-07-04 01:40:36 +0100

    feat: refactor prettier config(2)

---
+++ .prettierrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+	trailingComma: 'es5',
+	tabWidth: 4,
+	semi: true,
+	singleQuote: true,
+	bracketSpacing: false,
+	jsxBracketSameLine: true,
+	printWidth: 170,
+	useTabs: true,
+};
